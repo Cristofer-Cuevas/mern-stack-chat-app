22dcc5048a8e7d428b017b94f1b8a8a989bac86e
Christopher <cristoferperezmejia80@gmail.com>
2021-12-03 18:00:49 -0400

    SignIn page added and styled

---
+++ client/src/components/SignIn/SignIn.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// Styels Components
+import { Main } from "../styles/SignIn-styles";
+
+const SingIn = () => {
+  return (
+    <Main>
+      <h1>Sign In to start</h1>
+    </Main>
+  );
+};
+
+export default SingIn;
