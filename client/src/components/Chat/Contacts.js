9167da35368a8df303ff643ce1272ec11696d316
programmerXS <cristoferperezmejia80@gmail.com>
2022-03-22 12:00:39 -0400

    socket end-point added

--- client/src/components/Chat/Contacts.js
+++ client/src/components/Chat/Contacts.js
@@ -44,7 +44,7 @@ const Contacts = () => {
 
     //============================================//
     // -------------------- Socket State --------------------- //
-    const socket = io("https://mern-stack-chat-app-1.herokuapp.com", {
+    const socket = io("https://mern-stack-chat-app-1.herokuapp.com/", {
       extraHeaders: {
         Authorization: cookieValue,
       },
