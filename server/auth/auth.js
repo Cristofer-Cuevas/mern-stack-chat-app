1a61d4d7d5967e6d6e113a77e2da43bb5b4f729a
locastillo <locastillo@users.noreply.github.com>
2023-11-12 17:40:44 -0400

    callback romoved

--- server/auth/auth.js
+++ server/auth/auth.js
@@ -1,26 +1,26 @@
 import passport from "passport";
 import fs from "fs";
-import { ExtractJwt, Strategy } from "passport-jwt";
+import {ExtractJwt, Strategy} from "passport-jwt";
 import User from "../models/User.js";
 
 const JwtStrategy = Strategy;
 
-const PUB_KEY = fs.readFileSync("id_rsa_pub.pem", { encoding: "utf8" }, import.meta.url);
+const PUB_KEY = fs.readFileSync("id_rsa_pub.pem", {encoding: "utf8"}, import.meta.url);
 
 const options = {
-  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-  secretOrKey: PUB_KEY,
-  algorithms: ["RS256"],
+    jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+    secretOrKey: PUB_KEY,
+    algorithms: ["RS256"],
 };
 
 passport.use(
-  new JwtStrategy(options, (payload, done) => {
-    User.findOne({ _id: payload.sub }, (err, user) => {
-      if (user) {
-        done(null, user);
-      } else {
-        done(err, false);
-      }
-    });
-  })
+    new JwtStrategy(options, async (payload, done) => {
+
+        const user = await User.findOne({_id: payload.sub});
+        if (user) {
+            done(null, user);
+        } else {
+            done(null, false);
+        }
+    })
 );
